fdac2e40b4d5fecd9b195597f89b311d5c0e4693
Azureki <fzureice@gmail.com>
2018-09-09 21:50:03 +0800

    添加注释

--- lc892-2.py
+++ lc892-2.py
@@ -4,6 +4,9 @@ class Solution:
         :type grid: List[List[int]]
         :rtype: int
         """
+
+        # 外面，包括凹下去的。
+        # 欣宜还是考虑复杂了，负的也没关系，用绝对值就行了。不需要两个方向都跑一次。
         def helper(heights):
             cur = 0
             ret = 0
@@ -14,11 +17,14 @@ class Solution:
             return ret
 
         area = 0
+        # 每一行
         for row in grid:
             area += helper(row)
 
+        # 每一列
         for col in zip(*grid):
             area += helper(col)
 
+        # 上下底面
         area += sum(2 for row in grid for h in row if h > 0)
         return area
