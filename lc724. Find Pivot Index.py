e3c0ebdc5d3072777e24bc62c22ffb855548167e
Azureki <fzureice@gmail.com>
2018-09-18 11:15:44 +0800

    简化代码

--- lc724. Find Pivot Index.py
+++ lc724. Find Pivot Index.py
@@ -4,24 +4,19 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        n = len(nums)
-        if n == 0:
-            return -1
-
-        lsum = nums[0]
+        lsum = 0
         rsum = sum(nums)
-        if lsum == rsum:
-            return 0
-        for i in range(1, n):
+        for i in range(len(nums)):
             lsum += nums[i]
-            rsum -= nums[i - 1]
+
             if lsum == rsum:
                 return i
+            rsum -= nums[i]
         else:
             return -1
 
 
 # test
-nums = [-1, -1, -1, 0, 1, 1]
+nums = [1, 7, 3, 6, 5, 6]
 s = Solution()
 print(s.pivotIndex(nums))
