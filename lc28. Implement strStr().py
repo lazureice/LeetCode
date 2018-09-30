ecc15e23469b9e79982318f6f4b957b4834733b6
Azureki <fzureice@gmail.com>
2018-09-30 16:21:20 +0800

    手写

--- lc28. Implement strStr().py
+++ lc28. Implement strStr().py
@@ -5,7 +5,10 @@ class Solution:
         :type needle: str
         :rtype: int
         """
-        return haystack.find(needle)
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i + len(needle)] == needle:
+                return i
+        return -1
 
 
 haystack = "hello"
