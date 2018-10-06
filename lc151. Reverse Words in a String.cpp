e9e3f919b9d2e5917adf59042d48843119a28746
Azureki <fzureice@gmail.com>
2018-10-06 23:49:05 +0800

    去空格

--- lc151. Reverse Words in a String.cpp
+++ lc151. Reverse Words in a String.cpp
@@ -3,18 +3,36 @@ public:
     
     void reverseWords(string &s) {
         int i=0,j;
-        
+        while(i<s.size()&&s[i]==' '){
+            i++;
+        }
+        s.erase(s.begin(),s.begin()+i);
         while(i<s.size()){
-            while(i<s.size()&&!isalpha(s[i])){
+            
+            
+            while(i<s.size()&&!isalnum(s[i])){
                 i++;
             }
             j=i;
-            while(j!=s.size()&&isalpha(s[j])){
+            while(j!=s.size()&&isalnum(s[j])){
                 j++;
             }
             reverse(s.begin()+i,s.begin()+j);
             
             i=j;
+            while(i<s.size()&&s[i]!=' '){
+                i++;
+            }
+            j=i;
+            while(j<s.size()&&s[j]==' '){
+                j++;
+            }
+            if(j==s.size()){
+                s.erase(s.begin()+i,s.begin()+j);
+            }
+            else{
+                s.erase(s.begin()+i,s.begin()+j-1);
+            }
         }
         reverse(s.begin(),s.end());
     }
