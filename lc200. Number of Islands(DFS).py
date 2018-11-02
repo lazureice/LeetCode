761385642e2af275032327c7cb9a3637cbb07f26
Azureki <fzureice@gmail.com>
2018-11-02 23:38:42 +0800

    DFS Solution

---
+++ lc200. Number of Islands(DFS).py
@@ -0,0 +1,50 @@
+import queue
+
+
+class Solution:
+
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        def bfs(i, j):
+            # while not q.empty():
+
+            # isl = q.get()
+            if i < 0 or i >= length:
+                return
+            if j < 0 or j >= length0:
+                return
+            if not visited[i][j] and grid[i][j] == '1':
+                visited[i][j] = 1
+                bfs(i - 1, j)
+                bfs(i + 1, j)
+                bfs(i, j - 1)
+                bfs(i, j + 1)
+        if not grid:
+            return 0
+        res = 0
+        length = len(grid)
+        length0 = len(grid[0])
+
+        # q = queue.Queue()
+        visited = [[0] * length0 for i in range(length)]
+
+        for i in range(length):
+            for j in range(length0):
+                if not visited[i][j] and grid[i][j] == '1':
+                    bfs(i, j)
+                    res += 1
+
+        return res
+
+
+snum = '''11110
+11010
+11000
+00000'''
+slst = snum.split('\n')
+grid = [[int(x) for x in list(s)] for s in slst]
+soln = Solution()
+print(soln.numIslands(slst))
