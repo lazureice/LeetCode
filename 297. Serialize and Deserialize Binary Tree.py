445747626af3f0cbac05f5a849e659ed6bf29596
lazureice <lazureice@users.noreply.github.com>
2019-04-18 10:11:27 +0800

    Revert "AC. Use Queue. 384ms."
    This reverts commit 20877bbc98f0b5e9fd57ab59fe66a95037da4fa6.

--- 297. Serialize and Deserialize Binary Tree.py
+++ 297. Serialize and Deserialize Binary Tree.py
@@ -7,16 +7,14 @@
 
 from queue import Queue
 
-
 class Codec:
+
     def serialize(self, root):
         """Encodes a tree to a single string.
         
         :type root: TreeNode
         :rtype: str
         """
-        if not root:
-            return ''
         lst = []
         q = Queue()
         q.put(root)
@@ -29,6 +27,7 @@ class Codec:
             q.put(node.left)
             q.put(node.right)
         return ','.join(str(x) for x in lst)
+        
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -36,45 +35,19 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        if not data:
-            return None
-        lst = [
-            int(x) if x.lstrip('-').isdigit() else None
-            for x in data.split(',')
-        ]
-        q = Queue()
-        root = TreeNode(lst[0])
-        q.put(root)
-        length = len(lst)
-        idx = 0
-        while not q.empty():
-            node = q.get()
-            idx += 1
-            if idx == length:
-                break
-            if lst[idx] is not None:
-                left = TreeNode(lst[idx])
-                node.left = left
-                q.put(left)
-            idx += 1
-            if idx == length: break
-            if lst[idx] is not None:
-                right = TreeNode(lst[idx])
-                node.right = right
-                q.put(right)
+        def construct(lst, idx):
+            if lst[idx] is None:
+                return None
+            node = TreeNode(lst[idx])
+            left = idx * 2 + 1
+            right = idx * 2 + 2
+            if left < len(lst):
+                node.left = construct(lst, left)
+                node.right = construct(lst, right)
+            return node
 
-        return root
-
-        # def construct(lst, idx):
-        #     if lst[idx] is None:
-        #         return None
-        #     node = TreeNode(lst[idx])
-        #     left = idx * 2 + 1
-        #     right = idx * 2 + 2
-        #     if left < len(lst):
-        #         node.left = construct(lst, left)
-        #         node.right = construct(lst, right)
-        #     return node
+        lst = [int(x) if x.lstrip('-').isdigit() else None for x in data.split(',')]
+        return construct(lst,0)
 
 
 # Your Codec object will be instantiated and called as such:
