9bc2bdbc1d0e858a4244a440ac117aea06308912
lazureice <lazureice@users.noreply.github.com>
2019-04-18 10:11:27 +0800

    Revert "标记，但是仍然会重复 24/48 passed"
    This reverts commit d5bbaf1b4b98f70bf9e5803ea84982b1c435225f.

--- lc542. 01 Matrix.py
+++ lc542. 01 Matrix.py
@@ -1,5 +1,4 @@
 from queue import Queue
-from copy import deepcopy
 
 
 class Solution:
@@ -9,7 +8,6 @@ class Solution:
         :rtype: List[List[int]]
         """
         def bfs(i, j):
-            marked = deepcopy(visited)
             q = Queue()
             num = 0
             q.put((i, j))
@@ -17,21 +15,17 @@ class Solution:
                 size = q.qsize()
                 for _ in range(size):
                     mask = q.get()
-
-                    marked[mask[0]][mask[1]] = 1
                     if matrix[mask[0]][mask[1]] == 0:
                         return num
                     for r, c in [[-1, 0], [1, 0], [0, 1], [0, -1]]:
                         pos = mask[0] + r, mask[1] + c
                         if pos[0] > -1 and pos[0] < height:
                             if pos[1] > -1 and pos[1] < width:
-                                if marked[pos[0]][pos[1]] == 0:
-                                    q.put((pos[0], pos[1]))
+                                q.put((pos[0], pos[1]))
                 num += 1
 
         width = len(matrix[0])
         height = len(matrix)
-        visited = [[0] * width for _ in range(height)]
         for i in range(height):
             for j in range(width):
                 if matrix[i][j] == 0:
