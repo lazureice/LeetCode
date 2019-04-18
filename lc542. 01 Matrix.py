a2ca2d6ec97c986bfaa3eb4f557a6d39e675f4ee
lazureice <lazureice@users.noreply.github.com>
2019-04-18 10:11:27 +0800

    Revert "Queue改用list，AC"
    This reverts commit f1eb339f26b792a8d8c03dec33a4f0c7d678d734.

--- lc542. 01 Matrix.py
+++ lc542. 01 Matrix.py
@@ -1,29 +1,28 @@
-# from queue import Queue
+from queue import Queue
 
 
 class Solution:
     def updateMatrix(self, matrix):
         width = len(matrix[0])
         height = len(matrix)
-        q=[]
+        q = Queue()
         dirs = [(-1, 0), (1, 0), (0, 1), (0, -1)]
         for i in range(height):
             for j in range(width):
                 if matrix[i][j] == 0:
-                    q.append((i, j))
+                    q.put((i, j))
                 else:
                     matrix[i][j] = float('inf')
 
-        # while not q.empty():
-            # tem = q.get()
-        for tem in q:
+        while not q.empty():
+            tem = q.get()
             for d in dirs:
                 x, y = tem[0] + d[0], tem[1] + d[1]
                 if x < 0 or x >= height or y < 0 or y >= width or matrix[x][y] <= matrix[tem[0]][tem[1]]:
                     continue
                 else:
                     matrix[x][y] = matrix[tem[0]][tem[1]] + 1
-                    q.append((x, y))
+                    q.put((x, y))
         return matrix
 
 
